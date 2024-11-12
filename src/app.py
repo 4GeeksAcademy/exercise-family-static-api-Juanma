3dd0f916cb355c2b18bab5d8c5c9fc97bdd84546
Bertitom <juanbertoletti2@gmail.com>
2024-11-12 00:20:47 +0000

    new commit

--- src/app.py
+++ src/app.py
@@ -36,8 +36,35 @@ def handle_hello():
     }
 
 
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_members_by_id(id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+    for people in members:
+        if id== people.get("id"):
+            return jsonify(people), 200
+   
+    return jsonify({"msj":"not found"}), 404 
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.get_json()
+    new_member= jackson_family.add_member(request_body)
+    return jsonify(new_member), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_members_by_id(id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(id)
+    if member != None:
+        return jsonify({"done":True}), 200
+   
+    return jsonify({"msj":"not found"}), 404
+   
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
